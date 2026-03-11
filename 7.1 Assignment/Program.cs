28b409804b68ea067ee11df188d9e88d977dc1ff
362Sadie-F <princessmae.fleming@gmail.com>
2026-03-11 13:07:59 -0400

    7.1.1

--- 7.1 Assignment/Program.cs
+++ 7.1 Assignment/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace _7._1_Assignment
+﻿using System.ComponentModel.Design;
+using System.Runtime.CompilerServices;
+
+namespace _7._1_Assignment
 {
     internal class Program
     {
@@ -6,16 +9,35 @@
         {
             int numValues;
             int count = 0;
+            int total = 0;
+            int numbers;
+            int input;
+            bool done = true;
+            Random generator = new Random();
 
             //Average 
 
+            Console.Write("How many numbers are you entering?: ");
+            numbers = Convert.ToInt32(Console.ReadLine());
+           
+            while (count < numbers)
+            
+            {
+                Console.WriteLine("Please enter a number.");
+                numbers = Convert.ToInt32(Console.ReadLine());
+                total += numbers;
+                count++;
+            }
+            if (count == numbers);
+            {
+                done = true;
+            }
 
-          
             //Hi Low
-            
-          
-            
-            Console.WriteLine();
+
+
+
+           // Console.WriteLine();
         }
     }
 }
